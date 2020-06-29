0c740152eed1ef9439dafcc2df93b52579259aca
jrfeng <jrfeng331@qq.com>
2020-06-29 09:18:38 +0800

    refactor 'MediaButtonHelper'

--- helper/src/main/java/media/helper/MediaButtonHelper.java
+++ helper/src/main/java/media/helper/MediaButtonHelper.java
@@ -35,32 +35,23 @@ public class MediaButtonHelper extends BroadcastReceiver {
         return !Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction());
     }
 
-    /**
-     * 处理媒体按钮事件。如果媒体事件已被处理，则返回 true，否则返回 false。
-     *
-     * @param context  Context 对象。
-     * @param intent   要处理的媒体事件。
-     * @param listener 媒体事件监听器。
-     * @return 如果媒体事件已被处理，则返回 true，否则返回 false（如果 Intent 中不包含媒体事件，也会返回 false）。
-     */
-    public static boolean handleMediaButton(Context context, Intent intent, OnMediaButtonActionListener listener) {
+    public static boolean handleMediaButton(Context context, Intent intent,
+                                            MediaButtonEventCallback callback) {
         if (notMediaButtonAction(intent)) {
             return false;
         }
 
-        if (listener == null) {
+        if (callback == null) {
             return false;
         }
 
-        listener.onMediaButtonAction(context, intent);
-
-        return true;
+        return callback.handleMediaButtonEvent(context, intent);
     }
 
     /**
      * 将 {@link MediaButtonHelper} 注册为媒体按钮事件接收器。
      */
-    public static void registerMediaButtonReceiver(Context context) {
+    public static void registerMediaButtonEventReceiver(Context context) {
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         if (audioManager == null) {
             throw new IllegalStateException("AudioManager is null");
@@ -72,7 +63,7 @@ public class MediaButtonHelper extends BroadcastReceiver {
     /**
      * 取消将 {@link MediaButtonHelper} 注册为媒体按钮事件接收器。
      */
-    public static void unregisterMediaButtonReceiver(Context context) {
+    public static void unregisterMediaButtonEventReceiver(Context context) {
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         if (audioManager == null) {
             throw new IllegalStateException("AudioManager is null");
@@ -81,20 +72,10 @@ public class MediaButtonHelper extends BroadcastReceiver {
         audioManager.unregisterMediaButtonEventReceiver(new ComponentName(context, MediaButtonHelper.class));
     }
 
-    /**
-     * 回调接口。当检测到某个媒体按钮被触发时，该回调接口中的方法会被调用。
-     */
-    public interface OnMediaButtonActionListener {
-        void onMediaButtonAction(Context context, Intent intent);
-    }
-
-    /**
-     * 多媒体回调接口。该类是个抽象类，它实现了 {@link OnMediaButtonActionListener} 接口，并对多媒体相关的媒体按钮事件进行了封装。
-     */
-    public abstract static class MediaListener implements OnMediaButtonActionListener {
+    public abstract static class MediaButtonEventCallback {
         private ClickCounter mClickCounter;
 
-        public MediaListener() {
+        public MediaButtonEventCallback() {
             mClickCounter = new ClickCounter(new ClickCounter.Callback() {
                 @Override
                 public void onClick(int clickCount) {
@@ -103,16 +84,19 @@ public class MediaButtonHelper extends BroadcastReceiver {
             });
         }
 
-        @Override
-        public void onMediaButtonAction(Context context, Intent intent) {
+        boolean handleMediaButtonEvent(Context context, Intent intent) {
+            if (onMediaButtonEvent(context, intent)) {
+                return true;
+            }
+
             KeyEvent keyEvent = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
             if (keyEvent == null) {
-                return;
+                return false;
             }
 
             if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_HEADSETHOOK) {
                 mClickCounter.putEvent();
-                return;
+                return false;
             }
 
             mClickCounter.reset();
@@ -120,61 +104,74 @@ public class MediaButtonHelper extends BroadcastReceiver {
             switch (keyEvent.getKeyCode()) {
                 case KeyEvent.KEYCODE_MEDIA_PLAY:
                     onPlay();
-                    break;
+                    return true;
                 case KeyEvent.KEYCODE_MEDIA_PAUSE:
                     onPause();
-                    break;
+                    return true;
                 case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
                     onPlayPause();
-                    break;
+                    return true;
                 case KeyEvent.KEYCODE_MEDIA_STOP:
                     onStop();
-                    break;
+                    return true;
                 case KeyEvent.KEYCODE_MEDIA_NEXT:
                     onNext();
-                    break;
+                    return true;
                 case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
                     onPrevious();
-                    break;
+                    return true;
             }
+
+            return false;
+        }
+
+        public boolean onMediaButtonEvent(Context context, Intent intent) {
+            return false;
         }
 
         /**
          * "播放" 键被触发。
          */
-        public abstract void onPlay();
+        public void onPlay() {
+        }
 
         /**
          * "暂停" 键被触发。
          */
-        public abstract void onPause();
+        public void onPause() {
+        }
 
         /**
          * "播放/暂停" 键被触发。
          */
-        public abstract void onPlayPause();
+        public void onPlayPause() {
+        }
 
         /**
          * "停止" 键被触发。
          */
-        public abstract void onStop();
+        public void onStop() {
+        }
 
         /**
          * "下一曲" 键被触发。
          */
-        public abstract void onNext();
+        public void onNext() {
+        }
 
         /**
          * "上一曲" 键被触发。
          */
-        public abstract void onPrevious();
+        public void onPrevious() {
+        }
 
         /**
          * 耳机上的按钮被点击（可用于实现 "线控播放" 功能）。
          *
          * @param clickCount 按钮被点击的次数。
          */
-        public abstract void onHeadsetHookClicked(int clickCount);
+        public void onHeadsetHookClicked(int clickCount) {
+        }
     }
 
     private static class ClickCounter {
